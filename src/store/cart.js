4fa8dbd2322b8ca59c65612bacc94bb50bc12d1e
Francisco Agustin Munoz <franmunoz2501@gmail.com>
2024-09-23 11:23:55 -0300

    new carousel and frontend

--- src/store/cart.js
+++ src/store/cart.js
@@ -5,7 +5,7 @@ const getInitialCart = () => {
     const storedCart = localStorage.getItem('cart');
     return storedCart ? JSON.parse(storedCart) : [];
   }
-  return []; 
+  return [];
 };
 
 export const cart = atom(getInitialCart());
@@ -21,7 +21,12 @@ export const addToCart = (product) => {
       id: product.id,
       name: product.name,
       price: product.price,
-      quantity: 1
+      quantity: 1,
+      alt: product.alt,
+      description: product.description,
+      recomendation: product.recomendation,
+      summary: product.summary,
+      image: product.image,
     });
   }
 
@@ -37,3 +42,15 @@ export const removeFromCart = (productId) => {
   cart.set([...updatedCart]);
   localStorage.setItem('cart', JSON.stringify(updatedCart));
 };
+
+export const updateQuantity = (productId, quantity) => {
+  const currentCart = cart.get();
+  const existingProductIndex = currentCart.findIndex(item => item.id === productId);
+
+  if (existingProductIndex !== -1) {
+    currentCart[existingProductIndex].quantity = quantity;
+  }
+
+  cart.set([...currentCart]);
+  localStorage.setItem('cart', JSON.stringify(currentCart));
+}
