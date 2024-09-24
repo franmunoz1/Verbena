7a361be2c5102eb4ff90b405fd11bc4b3a793be3
alesso-chiavarino <alessochiavarino@gmail.com>
2024-09-24 17:17:26 -0300

    Add langs

---
+++ src/i18n/ui.ts
@@ -0,0 +1,18 @@
+export const languages = {
+    en: 'English',
+    es: 'Español'
+  };
+  
+  export const defaultLang = 'es';
+  
+  export const ui = {
+    en: {
+      'nav.home': 'Home',
+      'nav.about': 'About',
+      'nav.twitter': 'Twitter',
+    },
+    es: {
+      'nav.home': 'PROFE',
+      'nav.about': 'À propos',
+    },
+  } as const;
